0c4654806e531832c3c5ebc4a235342bc7286393
Guilherme <guilherme@spotify.com>
2025-11-03 14:04:16 +0100

    Compress results of reward experiments

--- src/drmdp/workflows/rewest.py
+++ src/drmdp/workflows/rewest.py
@@ -1,5 +1,6 @@
 import argparse
 import dataclasses
+import gzip
 import itertools
 import json
 import logging
@@ -962,20 +963,59 @@ def setup_experiment(exp_instance: core.ExperimentInstance):
     return env, algorithm, monitor, opt_logs
 
 
-def write_records(output_path: str, records):
+def write_records(
+    output_path: str,
+    records: Sequence[Mapping[str, Any]],
+    gzip_compression: bool = True,
+) -> None:
     """
     Exports records into JSON files.
     """
     bytes_size = sys.getsizeof(records)
+    if gzip_compression and not output_path.endswith(".gzip"):
+        output_path = ".".join([output_path, "gzip"])
+
     logging.info(
         "Writing partition of %fMB to %s",
         bytes_size / 1024 / 1024,
         output_path,
     )
-    with tf.io.gfile.GFile(output_path, "w") as writable:
-        for record in records:
-            json.dump(record, writable)
-            writable.write("\n")
+
+    if gzip_compression:
+        with tf.io.gfile.GFile(output_path, "wb") as writable:
+            with gzip.GzipFile(fileobj=writable, mode="wb") as writer:
+                for record in records:
+                    content = "".join([json.dumps(record), "\n"])
+                    writer.write(content.encode("UTF-8"))
+    else:
+        with tf.io.gfile.GFile(output_path, "w") as writable:
+            for record in records:
+                json.dump(record, fp=writable)
+                writable.write("\n")
+
+
+def read_records(
+    input_path: str, gzip_compression: bool = True
+) -> Sequence[Mapping[str, Any]]:
+    """
+    Read records from JSON.
+    """
+    logging.info(
+        "Reading file %s",
+        input_path,
+    )
+
+    records = []
+    if gzip_compression:
+        with tf.io.gfile.GFile(input_path, "rb") as readable:
+            with gzip.GzipFile(fileobj=readable, mode="rb") as reader:
+                for line in reader:
+                    records.append(json.loads(line.decode("UTF-8")))
+    else:
+        with tf.io.gfile.GFile(input_path, "r") as readable:
+            for line in readable:
+                records.append(json.loads(line))
+    return records
 
 
 def main():
