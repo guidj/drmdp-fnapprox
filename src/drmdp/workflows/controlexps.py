7e0ba2df2baafda21289b574df9f14a498b26108
Guilherme <guilherme@spotify.com>
2025-11-06 20:39:42 +0100

    Adds upper bound for options delay

--- src/drmdp/workflows/controlexps.py
+++ src/drmdp/workflows/controlexps.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import Any, Mapping, Sequence
+from typing import Any, Mapping, Optional, Sequence
 
 from drmdp import mathutils
 
@@ -17,13 +17,13 @@ MINES_GW_GRID = [
     "ooxooooooxoo",
     "sxxxxxxxxxxg",
 ]
-MAX_OPTIONS_DELAY = 3
+MAX_OPTIONS_DELAY = 4
 
 
 def discrete_least_specs(
     attempt_estimation_episodes: Sequence[int],
     feats_specs: Sequence[Mapping[str, Any]],
-    delays: Sequence[int] = (2, 3, 4, 6),
+    delays: Sequence[int] = (2, 4, 6),
     discounts: Sequence[float] = (1.0, 0.99),
 ):
     """
@@ -57,7 +57,7 @@ def discrete_least_specs(
 def least_specs(
     attempt_estimation_episodes: Sequence[int],
     feats_specs: Sequence[Mapping[str, Any]],
-    delays: Sequence[int] = (2, 3, 4, 6),
+    delays: Sequence[int] = (2, 4, 6),
     discounts: Sequence[float] = (1.0, 0.99),
 ) -> Sequence[Mapping[str, Any]]:
     """
@@ -91,7 +91,7 @@ def least_specs(
 def bayes_least_specs(
     init_attempt_estimation_episodes: Sequence[int],
     feats_specs: Sequence[Mapping[str, Any]],
-    delays: Sequence[int] = (2, 3, 4, 6),
+    delays: Sequence[int] = (2, 4, 6),
     discounts: Sequence[float] = (1.0, 0.99),
 ) -> Sequence[Mapping[str, Any]]:
     """
@@ -125,7 +125,7 @@ def bayes_least_specs(
 def cvlps_specs(
     attempt_estimation_episodes: Sequence[int],
     feats_specs: Sequence[Mapping[str, Any]],
-    delays: Sequence[int] = (2, 3, 4, 6),
+    delays: Sequence[int] = (2, 4, 6),
     discounts: Sequence[float] = (1.0, 0.99),
 ) -> Sequence[Mapping[str, Any]]:
     """
@@ -160,7 +160,7 @@ def cvlps_specs(
 def recurring_cvlps(
     init_attempt_estimation_episodes: Sequence[int],
     feats_specs: Sequence[Mapping[str, Any]],
-    delays: Sequence[int] = (2, 3, 4, 6),
+    delays: Sequence[int] = (2, 4, 6),
     discounts: Sequence[float] = (1.0, 0.99),
 ):
     """
@@ -193,7 +193,7 @@ def recurring_cvlps(
 
 
 def common_problem_specs(
-    delays: Sequence[int] = (2, 3, 4, 6), discounts: Sequence[float] = (1.0, 0.99)
+    delays: Sequence[int] = (2, 4, 6), discounts: Sequence[float] = (1.0, 0.99)
 ):
     """
     Specs that apply to every env.
@@ -212,13 +212,12 @@ def common_problem_specs(
         )
 
         for delay in delays:
-            delay_config = poisson_delay_config(delay)
             specs.extend(
                 [
                     {
                         "policy_type": "drop-missing",
                         "reward_mapper": {"name": "identity", "args": None},
-                        "delay_config": delay_config,
+                        "delay_config": poisson_delay_config(delay),
                         "epsilon": EPSILON,
                         "gamma": gamma,
                         "learning_rate_config": LEARNING_RATE_SPEC,
@@ -226,36 +225,32 @@ def common_problem_specs(
                     {
                         "policy_type": "markovian",
                         "reward_mapper": {"name": "zero-impute", "args": None},
-                        "delay_config": delay_config,
+                        "delay_config": poisson_delay_config(delay),
+                        "epsilon": EPSILON,
+                        "gamma": gamma,
+                        "learning_rate_config": LEARNING_RATE_SPEC,
+                    },
+                    # These configs are memory intensive
+                    # even with moderate delays.
+                    # Limit upper bound
+                    {
+                        "policy_type": "options",
+                        "reward_mapper": {"name": "identity", "args": None},
+                        "delay_config": poisson_delay_config(delay, max_delay=delay),
+                        "epsilon": EPSILON,
+                        "gamma": 1.0,
+                        "learning_rate_config": LEARNING_RATE_SPEC,
+                    },
+                    {
+                        "policy_type": "single-action-options",
+                        "reward_mapper": {"name": "identity", "args": None},
+                        "delay_config": poisson_delay_config(delay, max_delay=delay),
                         "epsilon": EPSILON,
                         "gamma": gamma,
                         "learning_rate_config": LEARNING_RATE_SPEC,
                     },
                 ]
             )
-            # These configs are memory intensive
-            # even with moderate delays.
-            if delay <= MAX_OPTIONS_DELAY:
-                specs.extend(
-                    [
-                        {
-                            "policy_type": "options",
-                            "reward_mapper": {"name": "identity", "args": None},
-                            "delay_config": delay_config,
-                            "epsilon": EPSILON,
-                            "gamma": 1.0,
-                            "learning_rate_config": LEARNING_RATE_SPEC,
-                        },
-                        {
-                            "policy_type": "single-action-options",
-                            "reward_mapper": {"name": "identity", "args": None},
-                            "delay_config": delay_config,
-                            "epsilon": EPSILON,
-                            "gamma": gamma,
-                            "learning_rate_config": LEARNING_RATE_SPEC,
-                        },
-                    ]
-                )
     return tuple(specs)
 
 
@@ -449,7 +444,7 @@ def experiment_specs() -> Sequence[Mapping[str, Any]]:
             "name": "GridWorld-v0",
             "args": {"grid": MINES_GW_GRID, "max_episode_steps": 200},
             "feats_specs": [
-                {"name": "tiles", "args": {"tiling_dim": 6}},
+                {"name": "tiles", "args": {"tiling_dim": 7}},
             ],
             "problem_specs": common_problem_specs()
             + discrete_least_specs(
@@ -458,25 +453,25 @@ def experiment_specs() -> Sequence[Mapping[str, Any]]:
             )
             + least_specs(
                 attempt_estimation_episodes=(50,),
-                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 6}}],
+                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 7}}],
             )
             + cvlps_specs(
                 attempt_estimation_episodes=(50,),
-                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 6}}],
+                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 7}}],
             )
             + bayes_least_specs(
                 init_attempt_estimation_episodes=(10,),
-                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 6}}],
+                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 7}}],
             )
             + recurring_cvlps(
                 init_attempt_estimation_episodes=(10,),
-                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 6}}],
+                feats_specs=[{"name": "tiles", "args": {"tiling_dim": 7}}],
             ),
             "epochs": 5,
         },
         {
             "name": "IceWorld-v0",
-            "args": {"map_name": "8x8"},
+            "args": {"map_name": "8x8", "max_episode_steps": 200},
             "feats_specs": [
                 {"name": "tiles", "args": {"tiling_dim": 7}},
             ],
@@ -507,7 +502,7 @@ def experiment_specs() -> Sequence[Mapping[str, Any]]:
     return tuple(specs)
 
 
-def poisson_delay_config(lam: int):
+def poisson_delay_config(lam: int, max_delay: Optional[int] = None):
     """
     Natural Poisson bounds:
     low, lambda, high
@@ -520,4 +515,7 @@ def poisson_delay_config(lam: int):
     3 8 14
     """
     lb, _ = mathutils.poisson_exact_confidence_interval(lam)
-    return {"name": "clipped-poisson", "args": {"lam": lam, "min_delay": max(2, lb)}}
+    return {
+        "name": "clipped-poisson",
+        "args": {"lam": lam, "min_delay": max(2, lb), "max_delay": max_delay},
+    }
